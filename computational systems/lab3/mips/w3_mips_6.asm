565693aafc6b7e2d3729f7013811abd05c1d18b6
Mark Podkolzin <pma2pro@gmail.com>
2019-11-26 15:37:02 +0300

    Update w3_mips_6.asm

--- computational systems/lab3/mips/w3_mips_6.asm
+++ computational systems/lab3/mips/w3_mips_6.asm
@@ -5,7 +5,6 @@ S: .float 0
 str: .asciiz "S = "
 
 .text
-.globl main
 main:
 l.s $f1, one
 
@@ -14,10 +13,12 @@ sqrt.s $f0, $f0      # sqrt(A)
 add.s $f0, $f0, $f1  # sqrt(A) + 1
 swc1 $f0, S
 
+# print "S = "
 li $v0, 4
 la $a0, str
 syscall
 
+# print $f12 value
 lwc1 $f12, S
 li $v0, 2
 syscall

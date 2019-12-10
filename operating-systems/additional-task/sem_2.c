5487a882150ab1a31d2bf0290b831f4549a0492c
Mark Podkolzin <pma2pro@gmail.com>
2019-12-10 15:42:21 +0300

    OS additional task #1. Init

---
+++ operating-systems/additional-task/sem_2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+int main(int argc, char* argv[]) {
+    int semid;
+    char pathname[] = "sem_1.c";
+    key_t key;
+    struct sembuf mybuf;
+
+    if ((key = ftok(pathname, 0)) < 0) {
+        perror("Can\'t generate key");
+        return -1;
+    }
+
+    if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
+        perror("Can\'t get semid");
+        return -1;
+    }
+
+    mybuf.sem_op = 1;
+    mybuf.sem_flg = 0;
+    mybuf.sem_num = 0;
+
+    if (semop(semid, &mybuf, 1) < 0) {
+        perror("Can\'t wait for condition");
+        return -1;
+    }
+
+    puts("Condition is set");
+
+    return 0;
+}
